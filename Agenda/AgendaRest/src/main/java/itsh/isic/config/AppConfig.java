440523472a8b0d10874b71fba9c7ac81df8ca487
Luis Fernando Negron <51353226+FernyNeg@users.noreply.github.com>
2023-03-29 01:06:58 -0600

    utilizacion de los servicios
    Se crea el ComponentScan a la ruta base del pom del rest y sus rutas donde buscara los servicios

--- Agenda/AgendaRest/src/main/java/itsh/isic/config/AppConfig.java
+++ Agenda/AgendaRest/src/main/java/itsh/isic/config/AppConfig.java
@@ -3,8 +3,10 @@ package itsh.isic.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"itsh.isic"})
 public class AppConfig extends SpringBootServletInitializer{
 	
 	public static void main(String[] args) {
