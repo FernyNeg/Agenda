a8751cec20a84f6b080e83770afcb858aa2e35ec
Luis Fernando Negron <51353226+FernyNeg@users.noreply.github.com>
2023-03-30 22:19:14 -0600

    Configuracion de Springboot
    Configuracion de spring boot para que pueda funcionar mediante implementaciones en el AppConfig

--- Agenda/AgendaRest/src/main/java/itsh/isic/config/AppConfig.java
+++ Agenda/AgendaRest/src/main/java/itsh/isic/config/AppConfig.java
@@ -2,15 +2,33 @@ package itsh.isic.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
-@ComponentScan({"itsh.isic"})
-public class AppConfig extends SpringBootServletInitializer{
-	
+@ComponentScan({ "itsh.isic" })
+public class AppConfig extends SpringBootServletInitializer {
+
 	public static void main(String[] args) {
 		SpringApplication.run(AppConfig.class, args);
+	}
+
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+		return new Angular2PathLocationStrategyCustomizer();
+	}
+
+	private static class Angular2PathLocationStrategyCustomizer
+			implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+		@Override
+		public void customize(ConfigurableServletWebServerFactory factory) {
+			factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
+		}
 	}
 
 }
